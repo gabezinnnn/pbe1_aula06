214f7674a1bc6cf46afde2a478795fd21e8ba132
Gabriel-2341 <gabrielpagamentos59@gmail.com>
2026-09-17 17:14:58 -0300

    up

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const rotaInicial = (req, res) => {
     res.json("Back-end respondendo")
 }
 
+const routes = require("./src/routes")
+
 //Configurações do servidor
 const app = express()
 app.use(cors())
@@ -14,6 +16,7 @@ const porta = 3000
 
 //Rotas
 app.get('/', rotaInicial)
+app.use(routes)
 
 app.listen(porta, () => {
     console.log(`Servidor respondendo em: http://localhost:${porta}`)
